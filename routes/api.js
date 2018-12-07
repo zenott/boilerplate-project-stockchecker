c6948a8c2b188524ed1f479419311534f72db06c
Glitch (fertile-sprout) <none>
2018-12-07 09:46:04 +0000

    Convert to async-await

--- routes/api.js
+++ routes/api.js
@@ -8,58 +8,53 @@
 
 'use strict';
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-var fetch = require('node-fetch');
+const expect = require('chai').expect;
+const MongoClient = require('mongodb');
+const fetch = require('node-fetch');
 
 const CONNECTION_STRING = process.env.DB;
+
+function dbConn() {
+  return MongoClient.connect(CONNECTION_STRING);
+}
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
+    .get(async function (req, res){
       const stock=req.query.stock;
       const like=req.query.like;
       const ip=req.ip;
-      let canLike=true;
       
       if(!Array.isArray(stock)){
         const url='https://api.iextrading.com/1.0/stock/'+stock+'/price'
-        fetch(url)
-          .then(response => response.text())
-          .then(text => {
-            if(text==='Unknown symbol'){
-              res.json('Unknown symbol');
-              return;
-            }
-            MongoClient.connect(CONNECTION_STRING)
-              .then(db => {
-                console.log('Successfully connected to database');
-                let updObj={$set: {stock: stock}};
-                let upsert=false;
-                if(like===true || like==="true") {
-                  updObj={$inc: {likes : 1}, $addToSet: {ips: ip}};
-                  upsert=true;
-                  db.collection('likes').findOne({stock: stock, ips: ip})
-                    .then(doc => {
-                      if(doc){
-                        canLike=false;
-                      }
-                      if(!canLike){
-                        updObj={$set: {stock: stock}};
-                        upsert=false;
-                      }
-                      db.collection('likes').findOneAndUpdate({stock: stock}, updObj, {upsert: upsert, returnOriginal: false})
-                        .then(doc => res.json({stockdata: {stock: stock, price: text, likes: (doc.value) ? doc.value.likes : 0}}));
-                    });
-                } else {
-                  db.collection('likes').findOneAndUpdate({stock: stock}, updObj, {upsert: upsert, returnOriginal: false})
-                    .then(doc => res.json({stockdata: {stock: stock, price: text, likes: (doc.value) ? doc.value.likes : 0}}));
-                }
-              });
+        const text = await fetch(url).then(response => response.text());
+        if(text==='Unknown symbol'){
+          res.json('Unknown symbol');
+          return;
+        }
+        const db= await dbConn();
+        let updObj={$set: {stock: stock}};
+        let upsert=false;
+        if(like===true || like==="true") {
+          updObj={$inc: {likes : 1}, $addToSet: {ips: ip}};
+          upsert=true;
+          const find= await db.collection('likes').findOne({stock: stock, ips: ip});
+          if(find){
+            updObj={$set: {stock: stock}};
+            upsert=false;
+          }
+          const doc = await db.collection('likes').findOneAndUpdate({stock: stock}, updObj, {upsert: upsert, returnOriginal: false});
+          res.json({stockdata: {stock: stock, price: text, likes: (doc.value) ? doc.value.likes : 0}});
+
+        } else {
+          const doc = await db.collection('likes').findOneAndUpdate({stock: stock}, updObj, {upsert: upsert, returnOriginal: false});
+          res.json({stockdata: {stock: stock, price: text, likes: (doc.value) ? doc.value.likes : 0}});
+        }
+              
             
-          })
-          .catch(error => console.error(error));
+          
+          
       } else {
         const urls=['https://api.iextrading.com/1.0/stock/'+stock[0]+'/price', 'https://api.iextrading.com/1.0/stock/'+stock[1]+'/price'];
         const promises=urls.map(url => fetch(url).then(response => response.text()).then(text=>text).catch(err => console.error(err)));
@@ -71,7 +66,7 @@ module.exports = function (app) {
                 db.collection('likes').findOne({stock: stock[0], ips: ip})
                   .then(doc => {
                     if(doc){
-                      canLike=false;
+                      
                     }
                   })
               })
