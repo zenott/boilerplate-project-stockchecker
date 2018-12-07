96b425a85c5df3ac6936de317bf9a9f53c0d34b8
Glitch (fertile-sprout) <none>
2018-12-07 14:23:41 +0000

    Add multiple stock function

--- routes/api.js
+++ routes/api.js
@@ -15,63 +15,70 @@ const fetch = require('node-fetch');
 const CONNECTION_STRING = process.env.DB;
 
 function dbConn() {
-  return MongoClient.connect(CONNECTION_STRING);
+  return MongoClient.connect(CONNECTION_STRING).catch(err => console.error(err));
 }
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(async function (req, res){
-      const stock=req.query.stock;
+      let stock=req.query.stock;
       const like=req.query.like;
       const ip=req.ip;
       
       if(!Array.isArray(stock)){
         const url='https://api.iextrading.com/1.0/stock/'+stock+'/price'
-        const text = await fetch(url).then(response => response.text());
+        const text = await fetch(url).then(response => response.text()).catch(err => console.error(err));
         if(text==='Unknown symbol'){
           res.json('Unknown symbol');
           return;
         }
         const db= await dbConn();
-        let updObj={$set: {stock: stock}};
-        let upsert=false;
         if(like===true || like==="true") {
-          updObj={$inc: {likes : 1}, $addToSet: {ips: ip}};
-          upsert=true;
-          const find= await db.collection('likes').findOne({stock: stock, ips: ip});
+          let updObj={$inc: {likes : 1}, $addToSet: {ips: ip}};
+          let upsert=true;
+          const find= await db.collection('likes').findOne({stock: stock, ips: ip}).catch(err => console.error(err));
           if(find){
             updObj={$set: {stock: stock}};
             upsert=false;
           }
-          const doc = await db.collection('likes').findOneAndUpdate({stock: stock}, updObj, {upsert: upsert, returnOriginal: false});
+          const doc = await db.collection('likes').findOneAndUpdate({stock: stock}, updObj, {upsert: upsert, returnOriginal: false}).catch(err => console.error(err));
           res.json({stockdata: {stock: stock, price: text, likes: (doc.value) ? doc.value.likes : 0}});
 
         } else {
-          const doc = await db.collection('likes').findOneAndUpdate({stock: stock}, updObj, {upsert: upsert, returnOriginal: false});
-          res.json({stockdata: {stock: stock, price: text, likes: (doc.value) ? doc.value.likes : 0}});
+          const doc = await db.collection('likes').findOne({stock: stock}).catch(err => console.error(err));
+          res.json({stockdata: {stock: stock, price: text, likes: (doc) ? doc.likes : 0}});
         }
               
             
           
           
       } else {
-        const urls=['https://api.iextrading.com/1.0/stock/'+stock[0]+'/price', 'https://api.iextrading.com/1.0/stock/'+stock[1]+'/price'];
+        stock=stock.slice(0,2);
+        const urls=stock.map(stockEle => 'https://api.iextrading.com/1.0/stock/'+stockEle+'/price');
         const promises=urls.map(url => fetch(url).then(response => response.text()).then(text=>text).catch(err => console.error(err)));
-        Promise.all(promises)
-          .then(function(prices) {
-            let likes=[0, 0];
-            MongoClient.connect(CONNECTION_STRING)
-              .then(db => {
-                db.collection('likes').findOne({stock: stock[0], ips: ip})
-                  .then(doc => {
-                    if(doc){
-                      
-                    }
-                  })
-              })
-            res.json({stockdata: [{stock: stock[0], price: prices[0], likes: likes[0]}, {stock: stock[1], price: prices[1], likes: likes[1]}]});
-          })
+        const prices = await Promise.all(promises);
+        const db = await dbConn();
+        if(like===true || like==="true"){
+          const docArr = await Promise.all(stock.map(async (stockEle) => {
+            let updObj={$inc: {likes : 1}, $addToSet: {ips: ip}};
+            let upsert=true;
+            const find= await db.collection('likes').findOne({stock: stockEle, ips: ip}).catch(err => console.error(err));
+            if(find){
+              updObj={$set: {stock: stockEle}};
+              upsert=false;
+            }
+            const doc = await db.collection('likes').findOneAndUpdate({stock: stockEle}, updObj, {upsert: upsert, returnOriginal: false}).catch(err => console.error(err));
+            return doc.value;
+          }));
+          res.json({stockdata: [{stock: stock[0], price: prices[0], likes: docArr[0].likes-docArr[1].likes}, {stock: stock[1], price: prices[1], likes: docArr[1].likes-docArr[0].likes}]});
+        } else {
+          const docArr = await Promise.all(stock.map(stockEle => db.collection('likes').findOne({stock: stockEle}))).catch(err => console.error(err));
+          let likes=[0, 0];
+          if(docArr[0]) likes[0] = docArr[0].likes;
+          if(docArr[1]) likes[1] = docArr[1].likes;
+          res.json({stockdata: [{stock: stock[0], price: prices[0], likes: likes[0]-likes[1]}, {stock: stock[1], price: prices[1], likes: likes[1]-likes[0]}]});
+        }
       }
     });
     
